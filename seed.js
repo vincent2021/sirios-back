58bbc435d7d72b4d8899e57614c2d9c59783814f
vincent2021 <vincent2021@gmail.com>
2020-01-13 10:33:45 +0100

    first commit

--- seed.js
+++ seed.js
@@ -27,14 +27,15 @@ const classif = ['NP', 'DR', 'CF', 'SD'];
 const orga = ['EMAA', 'EMA', 'EDG', 'CDEC', 'EMAT'];
 const bur = ['BPLANS', 'BPSA', 'BORG', 'BOPS', 'BPFD'];
 const ver = ['1.0', '1.1', '2', '5', ''];
+const author = ['Sophie Cabanes', 'Benoit Dorigny', 'Damien Sauget', 'Stéphane Madoeuf'];
 
 function randomFile(type) {
     const name = randomDate() + '_' + classif[rand(0 , 3)] + '_' + orga[rand(0 , 4)] + '_' + bur[rand(0 , 4)];
     if (type == 'doc') {
         const titre = 'Presentation';
-        const ver = doc_ext[rand(0 , 4)];
+        const version = ver[rand(0 , 4)];
         const ext = doc_ext[rand(0 , 3)]; 
-        return name + '_' + titre + '_' + ver + '.' + ext;
+        return name + '_' + titre + '_' + version + '.' + ext;
     } else if (type == 'img') {
         const titre = 'Carto';
         const ext = img_ext[rand(0 , 2)]; 
@@ -46,13 +47,13 @@ const root_folder = 'seed';
 const network = ['FROPS', 'INTRADEF', 'INTRACED'];
 const folder = ['BARKHANE', 'GPPO-PANDA', 'GPPO-OPX24', 'CPOIA', 'EDG', 'BMA']
 const subfolder = ['00_J3', '01_J5', '02_J35', '03_J1'];
-folder.forEach(function(folder) {
-    mkdirp(root_folder + '/' + folder)
-    subfolder.forEach(function(subfolder) {
-        mkdirp(root_folder + '/' + folder + '/' + subfolder);
-        mkdirp(root_folder + '/' + folder + '/' + subfolder + '/PUBLIC');
-    });
-});
+// folder.forEach(function(folder) {
+//     mkdirp(root_folder + '/' + folder)
+//     subfolder.forEach(function(subfolder) {
+//         mkdirp(root_folder + '/' + folder + '/' + subfolder);
+//         mkdirp(root_folder + '/' + folder + '/' + subfolder + '/PUBLIC');
+//     });
+// });
 function randFolder() {
     const path = network[rand(0 , 2)] + '/' + folder[rand(0 , 3)] + '/' + subfolder[rand(0 , 3)] + '/PUBLIC' ;
     return path
@@ -64,10 +65,12 @@ for (let i = 0; i < 500; i++) {
     const filename = randomFile('doc');
     const filepath = randFolder() + '/' + filename;
     const info = filename.split('_');
+    const aut = author[rand(0 , 3)];
     const ob = {
         'filepath': filepath,
         'filename': filename,
-        'info': info
+        'info': info,
+        'author': aut
     };
     bdd.push(ob);
 }
@@ -76,10 +79,12 @@ for (let i = 0; i < 200; i++) {
     const filename = randomFile('doc');
     const filepath = randFolder() + '/' + filename;
     const info = filename.split('_');
+    const aut = author[rand(0 , 3)];    
     const ob = {
         'filepath': filepath,
         'filename': filename,
-        'info': info
+        'info': info,
+        'author': aut
     };
     bdd.push(ob);
 }
